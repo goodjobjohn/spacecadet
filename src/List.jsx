1a47f52f02c36ee7ee1082b2f5cc5cc827263d8f
goodjobjohn <jpbash@gmail.com>
2024-07-16 21:48:30 +1000

    ~

--- src/List.jsx
+++ src/List.jsx
@@ -11,3 +11,17 @@ export default function List() {
       </div>
     );
   }
+
+  function Item() {
+    return (
+      <div className="item">
+       
+          <div class="title" contenteditable="true" data-text="+ Add title"></div>
+          <div class="delete close" contenteditable="false"></div>
+          <ul>
+            
+          </ul>
+                    
+      </div>
+    );
+  }
