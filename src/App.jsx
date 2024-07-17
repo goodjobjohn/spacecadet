aa5975365ad125f8fcff5deaf21d6d39d79b3f3f
goodjobjohn <jpbash@gmail.com>
2024-07-17 21:06:26 +1000

    swapped dnd to gridstack

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import List from './List.jsx';
-import './App.css'
+import React, { useEffect } from "react";
 
-export default function App() {
-  const [count, setCount] = useState(0)
+import { GridStack } from 'gridstack';
+import 'gridstack/dist/gridstack.css';
 
-  return (
-    <>          
-      <List />
-    </>
-  )
+import "./App.css";
+
+function App() {
+    // _________________________________________________
+    // Initialize Gridstack inside useEffect so that DOM is rendered when its initialized
+    // _________________________________________________
+    useEffect(() => {
+        var grid = GridStack.init();
+    });
+    // _________________________________________________
+    // _________________________________________________
+
+    return (
+        <div className="App">
+            <div className="grid-stack">
+                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="4">
+                    <div className="grid-stack-item-content">Item 1</div>
+                </div>
+                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="4">
+                    <div className="grid-stack-item-content">Item 2</div>
+                </div>
+                <div className="grid-stack-item border-dark" data-gs-width="4" data-gs-height="4">
+                    <div className="grid-stack-item-content">Item 3</div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-
+export default App;
