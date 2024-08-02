52154e26a63d54b5ed0b030269b3526b89dc58bc
goodjobjohn <john@basham.com.au>
2024-08-02 14:03:13 +1000

    Lists add button work

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const css = String.raw;
 
@@ -7,6 +7,7 @@ function App() {
         .app,
         .inbox,
         .lists,
+        .list,
         .schedule {
             border: 1px solid black;
             border-radius: 20px;
@@ -31,24 +32,57 @@ function App() {
             max-width: 250px;
             border-right: 1px solid black;
             border-bottom: 1px solid black;
+            word-break: break-word;
         }
         .lists {
             grid-area: lists;
+            display: flex;
             border-bottom: 1px solid black;
+            padding: 10px;
+            overflow-x: scroll;
+        }
+        .list {
+            min-width: 250px;
+            min-height: 250px;
         }
         .schedule {
             grid-area: schedule;
         }
     `;
 
+    const [listCount, setListCount] = useState(1);
+
+    const addList = () => {
+        setListCount(listCount + 1);
+    };
+
     return (
         <div className="app">
             <style>{allStyles}</style>
-            <div className="inbox"></div>
-            <div className="lists"></div>
+            <div className="inbox">
+                ajksdhajsihdas hdkljash dkja hskdj haskjdh aksjhdkaj
+                shdakjshdkajshdkajshdkajshdkajshdkjashdkajshdka
+            </div>
+            <div className="lists">
+                {Array.from({ length: listCount }, (_, index) => (
+                    <List key={index} />
+                ))}
+                <button onClick={addList}>Add</button>
+            </div>
             <div className="schedule"></div>
         </div>
     );
 }
 
 export default App;
+
+function List() {
+    return (
+        <div className="list">
+            <div class="item" contenteditable="true">
+                dsdsdsd
+            </div>
+            <button>Add</button>
+        </div>
+    );
+}
